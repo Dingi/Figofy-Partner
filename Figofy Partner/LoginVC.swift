d2e37bd723f86e810b6a9e291574501ae52ca639
Janus Ringling <janusringling2@gmail.com>
2016-04-26 10:43:06 +0200

    LoginView

--- Figofy Partner/LoginVC.swift
+++ Figofy Partner/LoginVC.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func login(sender: AnyObject) {
+        
+    }
 
 }
 
